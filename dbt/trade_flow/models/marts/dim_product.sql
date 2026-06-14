ef232beef2f327c4b44cc98a71f6a65f319cc41e
Zane <zaneschneider@gmail.com>
2026-06-14 11:28:21 -0700

    add product section seed, hs2/hs4 codes to dim

--- dbt/trade_flow/models/marts/dim_product.sql
+++ dbt/trade_flow/models/marts/dim_product.sql
@@ -5,11 +5,22 @@ products as (
     select * from {{ ref('stg_baci_product_codes') }}
 ),
 
+hs_sections as (
+    select * from {{ ref('hs_sections') }}
+),
+
 final as (
     select
-        hs6_product_code,
-        product_description
+        products.hs6_product_code,
+        products.product_description,
+        substr(products.hs6_product_code, 1, 2) as hs2,
+        substr(products.hs6_product_code, 1, 4) as hs4,
+        hs_sections.chapter_description,
+        hs_sections.hs_section,
+        hs_sections.section_description
     from products
+    left join hs_sections 
+        on substr(products.hs6_product_code, 1, 2) = lpad(cast(hs_sections.hs2 as string), 2, '0')
 )
 
 select * from final
